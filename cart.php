1ebb0336ff2eb996393d55a2e26ec39537a46921
asfk2003 <146013411+asfk2003@users.noreply.github.com>
2023-11-01 11:41:25 +0500

    Add files via upload
    added the list of some products

---
+++ cart.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/homepage.css">
+    <title>Cart</title>
+</head>
+<body>
+<div class="right">
+        <ul class="navbar">
+            <li><a href="homepage.php">Home</a></li>
+            <li><a href="cart.php">Cart</a></li>
+            <li><a href="about.php">About</a></li>
+          </ul>
+        </div>
+</body>
+</html>
